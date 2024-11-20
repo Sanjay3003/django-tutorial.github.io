49927cab4ddc4bdd570e17e3a084eabfe6d8bd2a
Harshil Sondagar <sondagarharshil0@gmail.com>
2024-11-21 02:46:45 +0530

    #

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3(^)dhn^pv5!ka$&a-_^9fgcy@p-l4l+1=!#ocklw-2_dk)brz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.app'
 
 
 # Database
